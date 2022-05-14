72611d06d323730dc08ecd0ef98e60bb773f18e1
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-05-14 17:21:15 +0200

    Remove old conf and add callback port as an env variable.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 const Configuration = {
     BASE_URL: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/`,
     GH_CLIENT_ID: process.env.REACT_APP_GITHUB_CLIENT_ID,
-    GH_REDIRECT_URL: `http://${process.env.REACT_APP_API_HOST}:3000`,
+    GH_REDIRECT_URL: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_CALLBACK_PORT}`,
     PATH: '/',
-    GH_CLIENT_SECRET: process.env.REACT_APP_GITHUB_CLIENT_SECRET
 }
 
 export default Configuration
