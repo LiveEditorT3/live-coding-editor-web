8d4cfce07ddcae1237a061b5d7587992371b1465
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-06-06 18:02:05 +0200

    Better document languages modes

--- src/models/languageModes.js
+++ src/models/languageModes.js
@@ -1,10 +1,18 @@
-// Select corresponding CodeMirror editor mode for the file name
+/**
+ * Select corresponding CodeMirror editor mode for the file name.
+ * @param {String} filename - the filename with extension
+ * @returns a valid CodeMirror mode
+ */
 export const selectEditorMode = (filename) => {
   const { extension } = extractFilenameAndExtension(filename);
   return fileExtensionToEditorMode(extension);
 };
 
-// Separate file name from file extension
+/**
+ * Separate file name from file extension.
+ * @param {String} filename - the filename with extension
+ * @returns {String, String} - an object with the filename and the extension
+ */
 export const extractFilenameAndExtension = (filename) => {
   const lastDotIndex = filename.lastIndexOf(".");
   // The file name starts with a dot or there is no dot, so there is no extension
@@ -18,10 +26,14 @@ export const extractFilenameAndExtension = (filename) => {
   }
 };
 
-// Convert a file extension to the corresponding mode
-// for the 'mode' parameter in CodeMirror.
-export const fileExtensionToEditorMode = (mode) => {
-  switch (mode) {
+/**
+ * Convert a file extension to the corresponding mode
+ * for the 'mode' parameter in CodeMirror.
+ * @param {String} extension - a file extension
+ * @returns {String} a valid CodeMirror mode
+ */
+export const fileExtensionToEditorMode = (extension) => {
+  switch (extension) {
     case "go":
       return "go";
     case "js":
