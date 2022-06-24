e6c1e1a96c7770e8370e9531c0d70fd0e76a9cb1
Fede <falvareza@fi.uba.ar>
2022-06-24 19:34:24 -0300

    firebase configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const FirebaseConfig = {
+  apiKey: "AIzaSyBE7bUoFdwuThT7Mv_dnCq_Pie9ym7UCFs",
+  authDomain: "live-coding-editor-5762f.firebaseapp.com",
+  projectId: "live-coding-editor-5762f",
+  storageBucket: "live-coding-editor-5762f.appspot.com",
+  messagingSenderId: "423933700967",
+  appId: "1:423933700967:web:b6cac8aff10d99097e5dc2",
+  measurementId: "G-0BLXSQE1R6"
+};
+
+export default FirebaseConfig
