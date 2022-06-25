4b77331f32d7377689ce7b1e9c10b5caa17413ac
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-06-25 23:46:36 +0200

    Simplify env var (host + port) to URL.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const Configuration = {
-  BASE_URL: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/`,
+  BASE_URL: process.env.REACT_APP_API_URL,
   GH_CLIENT_ID: process.env.REACT_APP_GITHUB_CLIENT_ID,
-  GH_REDIRECT_URL: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_CALLBACK_PORT}`,
-  PATH: "/",
 };
 
 export default Configuration;
