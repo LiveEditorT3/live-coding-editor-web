9d7be3dc77ee8d109f37713e92de9bde6278eecf
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-07-10 12:30:07 +0200

    Access localStorage directly in LoginProvider, remove unnecessary functions. Rewrite using async/await

--- src/hooks/login/index.jsx
+++ src/hooks/login/index.jsx
@@ -9,12 +9,8 @@ import { clearUser, saveUserInStorage } from "../user/useUser";
 import Configuration from "../../config";
 
 export const signOut = () => {
-  clearToken();
+  localStorage.removeItem(Configuration.TOKEN_KEY);
   clearUser();
-};
-
-export const clearToken = () => {
-  localStorage && localStorage.removeItem(Configuration.TOKEN_KEY);
   window.location.replace(window.location.origin);
 };
 
@@ -23,16 +19,12 @@ const getCode = () => {
   return params.get("code");
 };
 
-export const loggedIn = () => !!getTokenInStorage();
-
-export const getTokenInStorage = () =>
-  localStorage && localStorage.getItem(Configuration.TOKEN_KEY);
-
-export const saveTokenInStorage = (token, key = Configuration.TOKEN_KEY) =>
-  localStorage && localStorage.setItem(key, token);
+export const loggedIn = () => !!localStorage.getItem(Configuration.TOKEN_KEY);
 
 export const LoginProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(!!getTokenInStorage());
+  const [loggedIn, setLoggedIn] = useState(
+    !!localStorage.getItem(Configuration.TOKEN_KEY)
+  );
   const { login } = useSelector((store) => store.user.user.login);
   const { app } = useFirebaseContext();
   const dispatch = useDispatch();
@@ -41,11 +33,10 @@ export const LoginProvider = ({ children }) => {
     const getAccessToken = async () => {
       var gitCode = getCode();
       if (!!gitCode) {
-        userService.Login(gitCode).then((res) => {
-          setLoggedIn(!!res.access_token);
-          saveTokenInStorage(res.access_token);
-          window.location.replace(`${window.location.origin}/${v4()}`);
-        });
+        const res = await userService.Login(gitCode);
+        setLoggedIn(!!res.access_token);
+        localStorage.setItem(Configuration.TOKEN_KEY, res.access_token);
+        window.location.replace(`${window.location.origin}/${v4()}`);
       }
     };
 
@@ -55,25 +46,25 @@ export const LoginProvider = ({ children }) => {
   useEffect(() => {
     const localUser = JSON.parse(localStorage.getItem("user"));
 
-    const getUser = () => {
-      userService.GetUser().then((user) => {
-        if (!!user) {
-          const db = getDatabase(app);
-          saveUserInStorage({ user });
-          dispatch(setUser(user));
-          set(ref(db, `sessions${window.location.pathname}/${user.id}`), {
-            id: user.id,
-            name: user.name,
-            login: user.login,
-            write: loggedIn,
-            admin: loggedIn,
-          });
-        }
-      });
+    const getUser = async () => {
+      const user = await userService.GetUser();
+      if (!!user) {
+        const db = getDatabase(app);
+        saveUserInStorage({ user });
+        dispatch(setUser(user));
+        set(ref(db, `sessions${window.location.pathname}/${user.id}`), {
+          id: user.id,
+          name: user.name,
+          login: user.login,
+          write: loggedIn,
+          admin: loggedIn,
+        });
+      }
     };
 
-    if (loggedIn && !login) getUser();
-    else if (!!localUser && localUser.login !== login) {
+    if (loggedIn && !login) {
+      getUser();
+    } else if (!!localUser && localUser.login !== login) {
       const db = getDatabase(app);
       set(ref(db, `sessions${window.location.pathname}/${localUser.id}`), {
         id: localUser.id,
